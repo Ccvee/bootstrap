89e3692f12728bc8a10c17f4ab6e5bb321e5c3b1
Aki <aki.takanori@teldevice.co.jp>
2020-07-20 23:39:08 +0900

    update: added package, simplified things
    update: added package, simplified things

--- install_pkg.sh
+++ install_pkg.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+#
+# Ubuntu 20.04
+#
 
 echo 'Updating...'
 sudo apt update
-
 echo 'Installing packages...'
-sudo apt install -y neovim git fzf ssh zsh tmux python3-pip i3
+sudo apt install -y git ssh zsh neovim fzf tmux python3 python3-pip bat nodejs
+sudo npm install --global git-open
 
-# when tmux spacekey doesn't work. tmux-256 thing is not registerd
+# When tmux spacekey doesn't work. tmux-256 thing is not registerd
 # properly.
 sudo apt install -y ncurses-term
